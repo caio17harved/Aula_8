beeab07893768ffd2fb8b8786b25e80d08a51830
caio17harved <caio.croccia@gmail.com>
2022-04-01 19:23:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-auaxdt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-auaxdt)
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { LogComponent } from './log/log.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports:      [ BrowserModule, 
+    FormsModule,
+  RouterModule.forRoot(
+    [
+      {path: ``, component: HomeComponent},
+      {path: `tasks`, component: TasksComponent},
+      {path: `log`, component: LogComponent},
+    ]
+  ) ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent,
+    HomeComponent,
+    TasksComponent,
+    LogComponent
+  ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
